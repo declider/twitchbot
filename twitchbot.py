4e3bc98c7f323aa214ce51221d47162f6673661c
declider <62831308+declider@users.noreply.github.com>
2023-02-01 15:23:01 +0300

    Update twitchbot.py

--- twitchbot.py
+++ twitchbot.py
@@ -1,101 +1,125 @@
 from twitchio.ext import commands
-import requests, os, json
+import requests, os, json, time
 from dotenv import load_dotenv
+from datetime import timedelta
+from keep_alive import keep_alive
 
 load_dotenv()
+
 
 class Bot(commands.Bot):
 
     def __init__(self):
-        super().__init__(token=os.getenv("TWITCH_TOKEN"), prefix='?', initial_channels=['segall'])
+        super().__init__(token=os.getenv("TWITCH_TOKEN"),
+                         prefix='!',
+                         initial_channels=['segall', "roadhouse", "declider"])
 
-        self.rating_users = []
-        self.rating_scores = []
-        self.rating_running = False
-
-
+        self.data = {}
 
     async def event_ready(self):
         print(f'Logged in as | {self.nick}')
-
-
+        for channel in self.connected_channels:
+            self.data[channel.name] = {
+                "rating": {
+                    "status": False,
+                    "users": [],
+                    "scores": []
+                }
+            }
+        self.start = time.time()
 
     async def event_message(self, message):
+        ch = message.channel.name
         if message.echo:
             return
 
-        if self.rating_running:
-            if message.author.id not in self.rating_users:
+        if self.data[ch]["rating"]["status"]:
+            if message.author.id not in self.data[ch]["rating"]["users"]:
                 score = message.content.split(" ")[0].strip()
                 if score.isdigit():
-                    if int(score)>=0 and int(score)<=10:
-                        self.rating_scores.append(int(score))
-                        self.rating_users.append(message.author.id)
+                    if int(score) >= 0 and int(score) <= 10:
+                        self.data[ch]["rating"]["scores"].append(int(score))
+                        self.data[ch]["rating"]["users"].append(
+                            message.author.id)
 
         await self.handle_commands(message)
 
-        
-        
     @commands.cooldown(rate=1, per=600, bucket=commands.Bucket.channel)
     @commands.command(name="нарисуй", aliases=["рисунок"])
     async def draw(self, ctx: commands.Context):
         emote = ctx.message.content.split(" ")[-1]
-        url = "https://api.thefyrewire.com/twitch/pastebin/6itDyhwk?filter="+emote
+        url = "https://api.thefyrewire.com/twitch/pastebin/6itDyhwk?filter=" + emote
         r = requests.get(url)
 
-        if len(r.text)>=100:
-            await ctx.send(r.text[0:500])
+        if len(r.text) >= 100:
+            await ctx.send(r.text[0:499])
         else:
             await ctx.command._cooldowns[0].reset()
 
-
-
-    @commands.command(name="rating", aliases=["рейтинг","оценка"])
+    @commands.command(name="rating", aliases=["рейтинг", "оценка"])
     async def rating(self, ctx: commands.Context):
 
-        if not ctx.author.is_mod and not ctx.author.is_vip and ctx.author.name!="declider":
-            # ctx.command._cooldowns[0].reset()
+        ch = ctx.channel.name
+
+        if not ctx.author.is_mod and ctx.author.name != "declider":
             return
 
-        if self.rating_running:
-            self.rating_running = False
-            if len(self.rating_scores)!=0:
-                rating = sum(self.rating_scores)
-                score = round(rating/len(self.rating_scores), 2)
-                await ctx.send(f"/announce Чат поставил оценку {score}! Всего оценок: {len(self.rating_scores)}")
-                self.rating_scores.clear()
-                self.rating_users.clear()
+        if self.data[ch]["rating"]["status"]:
+            self.data[ch]["rating"]["status"] = False
+            if len(self.data[ch]["rating"]["scores"]) != 0:
+                rating = sum(self.data[ch]["rating"]["scores"])
+                length = len(self.data[ch]["rating"]["scores"])
+                score = round(rating / length, 2)
+                await ctx.send(
+                    f"/announce Чат поставил оценку {score}! Всего оценок: {length}"
+                )
+                self.data[ch]["rating"]["scores"].clear()
+                self.data[ch]["rating"]["users"].clear()
             else:
-                await ctx.send(f"Оценивание отключено.")
-            
+                await ctx.send("Оценивание отключено.")
 
         else:
-            self.rating_running = True
-            await ctx.send("/announce Началось оценивание по шкале от 1 до 10! Пиши свою оценку в чат.")
-    
-
+            self.data[ch]["rating"]["status"] = True
+            await ctx.send(
+                "/announce Началось оценивание по шкале от 1 до 10! Пиши свою оценку в чат."
+            )
 
     @commands.cooldown(rate=1, per=60, bucket=commands.Bucket.channel)
     @commands.command(name="emotecount", aliases=["countemote"])
     async def emotecount(self, ctx: commands.Context):
 
-        targetId = os.getenv("DEFAULT_USER_ID")
+        user = await ctx.channel.user()
+        ch = ctx.channel.name
 
-        bttv = json.loads(requests.get("https://api.betterttv.net/3/cached/users/twitch/"+targetId).text)
-        bttvCE = len(bttv['channelEmotes'])
-        bttvSE = len(bttv['sharedEmotes'])
+        bttv = json.loads(
+            requests.get(
+                f"https://api.betterttv.net/3/cached/users/twitch/{user.id}").
+            text)
+        bttv = len(bttv['channelEmotes']) + len(bttv['sharedEmotes'])
 
-        ffz = json.loads(requests.get('https://api.betterttv.net/3/cached/frankerfacez/users/twitch/'+targetId).text)
+        ffz = json.loads(
+            requests.get(
+                f'https://api.betterttv.net/3/cached/frankerfacez/users/twitch/{user.id}'
+            ).text)
         ffz = len(ffz)
 
-        seventv = json.loads(requests.get('https://api.7tv.app/v2/users/'+targetId+'/emotes').text)
+        seventv = json.loads(
+            requests.get(
+                f'https://api.7tv.app/v2/users/{user.id}/emotes').text)
         seventv = len(seventv)
 
-        emotesum = bttvCE + bttvSE + ffz + seventv
+        emotesum = bttv + ffz + seventv
 
-        await ctx.send(f"На канале segall всего {emotesum} смайлов: {bttvCE}/{bttvSE} личных/общих в BTTV, {ffz} в FFZ, {seventv} в 7TV.")
+        await ctx.send(
+            f"На канале {ch} всего {emotesum} смайлов: {bttv} в BTTV, {ffz} в FFZ, {seventv} в 7TV."
+        )
 
+    @commands.command(name="ping")
+    async def ping(self, ctx: commands.Context):
+        end = round(time.time() - self.start)
+        await ctx.send(f"Бот работает {timedelta(seconds=end)}")
 
 
 bot = Bot()
+keep_alive()
 bot.run()
